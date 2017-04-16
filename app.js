276edca5fc6a301467e41bec99238557ceaf9bc2
Emelie Johansson <emelietejohansson@gmail.com>
2017-04-16 21:29:40 +0200

    Read and write streams + pipes

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var http = require('http');
+var fs = require('fs');
+
+/*
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received: ');
+    myWriteStream.write(chunk);
+});
+*/
 
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey');
+
+    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+    var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+    myReadStream.pipe(res);
+
+    //res.end('Hey');
 });
 
 server.listen(3000, '127.0.0.1');
