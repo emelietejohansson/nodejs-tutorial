3c40900208143d203dc5413bde4c2750c032a648
Emelie Johansson <emelietejohansson@gmail.com>
2017-04-17 14:48:46 +0200

    Intro to the template engine ejs

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require('express');
-
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send('This is the home page :)');
+    res.sendFile(__dirname +'/index.html');
 });
 
 app.get('/contact', function(req, res){
-    res.send('This is the contact page :)');
+    res.sendFile(__dirname +'/contact.html');
 });
 
 app.get('/profile/:name', function(req, res){
-    res.send('You requested a profile with the name of ' + req.params.name);
+    var data = {age: 26, job: 'Coder'};
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
